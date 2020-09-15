61ab8ae10eac8d87872a9eb5d59332e2c39b9252
Jiseon Kim <jsid1561@gmail.com>
2020-09-15 15:37:02 +0900

    Create dog-years.js

---
+++ dog-years.js
@@ -0,0 +1,18 @@
+const myAge = 23; //myAge
+
+let earlyYears = 2; 
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2; 
+laterYears *= 4;
+
+console.log(`earlyYears = ${earlyYears} laterYears = ${laterYears}`);
+
+// Calculate my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Write my name
+const myName = 'Jiseon Kim'.toLowerCase();
+
+// Display my name and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
