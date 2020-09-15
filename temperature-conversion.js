0e13dc36d643d253781b722d4c70940e59e1265e
Jiseon Kim <jsid1561@gmail.com>
2020-09-15 15:20:03 +0900

    Create temperature-conversion.js

---
+++ temperature-conversion.js
@@ -0,0 +1,18 @@
+// today's degree in kelvin
+const kelvin = 0;
+
+// Calculate Celsius
+const celsius = kelvin - 273;
+
+// Calculate Fahrenheit
+let fahrenheit = celsius*(9/5) + 32;
+
+// round the number
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+// Calculate Newton
+let newton = celsius*(33/100);
+newton = math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
