2a165dd19a9deaaad5a1d70a1702e673afe5c9dc
Jiseon Kim <jsid1561@gmail.com>
2020-09-15 17:07:47 +0900

    Create eightball.js

---
+++ eightball.js
@@ -0,0 +1,31 @@
+let userName = 'J Kim';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion =  'Could I become a web developer?';
+console.log(`The user asked :${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random()*8);
+
+let eightBall = '';
+
+switch(randomNumber){
+  case 0 : eightBall ='It is certain';
+  break;
+  case 1 : eightBall = 'It is decidedly so';
+  break;
+  case 2 : eightBall = 'Reply hazy try again';
+  break;
+  case 3 : eightBall = 'Cannot predict now';
+  break;
+  case 4 : eightBall = 'Do not count on it';
+  break;
+  case 5 : eightBall = 'My sources say no';
+  break;
+  case 6 : eightBall = 'Outlook not so good';
+  break;
+  case 7 : eightBall = 'Signs point to yes';
+  break;
+}
+
+console.log(`The eight ball answered: ${eightBall}`);
